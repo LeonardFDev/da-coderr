725fde99a1fccb9d6eecde0c9b7842f953659274
LeonardFDev <203679381+LeonardFDev@users.noreply.github.com>
2026-08-06 12:49:48 +0200

    feat: fix: The test for the GET offer-details was createted.

--- freelance_app/tests/test_offer.py
+++ freelance_app/tests/test_offer.py
@@ -631,11 +631,11 @@ class OfferDetailDeleteTests(APITestCase):
         self.client = APIClient()
         self.client.credentials(HTTP_AUTHORIZATION= "Token " + self.token.key)
 
-        for offer in range(1, 3):
-            offer = Offer.objects.create(user= self.profile, title= f"Grafikdesign-Paket{offer}", image= None, description= f"Ein umfassendes Grafikdesign-Paket für Unternehmen{offer}.")
-            OfferDetail.objects.create(title= f"Basic Design{offer}", revisions= 2, delivery_time_in_days= 5, price= 100.99, features= [f"Logo Design{offer}", f"Visitenkarte{offer}"], offer_type= "basic", offer= offer)
-            OfferDetail.objects.create(title= f"Standard Design{offer}", revisions= 5, delivery_time_in_days= 7, price= 200, features= [f"Logo Design{offer}", f"Visitenkarte{offer}", f"Briefpapier{offer}"], offer_type= "standard", offer= offer)
-            OfferDetail.objects.create(title= f"Premium Design{offer}", revisions= 10, delivery_time_in_days= 10, price= 500, features= [f"Logo Design{offer}", f"Visitenkarte{offer}", f"Briefpapier{offer}", f"Flyer{offer}"], offer_type= "premium", offer= offer)
+        for offer_number in range(1, 3):
+            offer = Offer.objects.create(user= self.profile, title= f"Grafikdesign-Paket{offer_number}", image= None, description= f"Ein umfassendes Grafikdesign-Paket für Unternehmen{offer_number}.")
+            OfferDetail.objects.create(title= f"Basic Design{offer_number}", revisions= 2, delivery_time_in_days= 5, price= 100.99, features= [f"Logo Design{offer_number}", f"Visitenkarte{offer_number}"], offer_type= "basic", offer= offer)
+            OfferDetail.objects.create(title= f"Standard Design{offer_number}", revisions= 5, delivery_time_in_days= 7, price= 200, features= [f"Logo Design{offer_number}", f"Visitenkarte{offer_number}", f"Briefpapier{offer_number}"], offer_type= "standard", offer= offer)
+            OfferDetail.objects.create(title= f"Premium Design{offer_number}", revisions= 10, delivery_time_in_days= 10, price= 500, features= [f"Logo Design{offer_number}", f"Visitenkarte{offer_number}", f"Briefpapier{offer_number}", f"Flyer{offer_number}"], offer_type= "premium", offer= offer)
 
         self.offer = Offer.objects.all()[1]
         
@@ -674,3 +674,61 @@ class OfferDetailDeleteTests(APITestCase):
             
         status_code_with_message(self, response)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class offerdetailsDetailGetTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="testpassword", email="testuser@test.de")
+        self.profile = Profile.objects.create(user= self.user, username= self.user.username, email = self.user.email, type = "business", first_name= "Test", last_name="name")
+    
+        self.token = Token.objects.create(user = self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION= "Token " + self.token.key)
+
+        for offer_number in range(1, 3):
+            offer = Offer.objects.create(user= self.profile, title= f"Grafikdesign-Paket{offer_number}", image= None, description= f"Ein umfassendes Grafikdesign-Paket für Unternehmen{offer_number}.")
+            OfferDetail.objects.create(title= f"Basic Design{offer_number}", revisions= 2, delivery_time_in_days= 5, price= 100.99, features= [f"Logo Design{offer_number}", f"Visitenkarte{offer_number}"], offer_type= "basic", offer= offer)
+            OfferDetail.objects.create(title= f"Standard Design{offer_number}", revisions= 5, delivery_time_in_days= 7, price= 200, features= [f"Logo Design{offer_number}", f"Visitenkarte{offer_number}", f"Briefpapier{offer_number}"], offer_type= "standard", offer= offer)
+            OfferDetail.objects.create(title= f"Premium Design{offer_number}", revisions= 10, delivery_time_in_days= 10, price= 500, features= [f"Logo Design{offer_number}", f"Visitenkarte{offer_number}", f"Briefpapier{offer_number}", f"Flyer{offer_number}"], offer_type= "premium", offer= offer)
+
+        self.offer = Offer.objects.all()[1]
+        self.offer_detail_basic = OfferDetail.objects.all()[3]
+        self.offer_detail_standard = OfferDetail.objects.all()[4]
+        self.offer_detail_premium = OfferDetail.objects.all()[5]
+        
+    def test_offer_details_detail_get_401(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token invalidtoken")
+
+        url = reverse("offerdetails-detail", kwargs={"id": self.offer_detail_basic.id}) 
+        response = self.client.get(url)
+                    
+        status_code_with_message(self, response)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_offer_details_detail_get_404(self):
+        url = reverse("offerdetails-detail", kwargs={"id": 999})
+        response = self.client.get(url)
+                        
+        status_code_with_message(self, response)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_offer_details_detail_basic_get_200(self):
+        url = reverse("offerdetails-detail", kwargs={"id": self.offer_detail_basic.id})
+        response = self.client.get(url)
+            
+        status_code_with_message(self, response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_offer_details_detail_standard_get_200(self):
+        url = reverse("offerdetails-detail", kwargs={"id": self.offer_detail_standard.id})
+        response = self.client.get(url)
+            
+        status_code_with_message(self, response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_offer_details_detail_premium_get_200(self):
+        url = reverse("offerdetails-detail", kwargs={"id": self.offer_detail_premium.id})
+        response = self.client.get(url)
+            
+        status_code_with_message(self, response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)    

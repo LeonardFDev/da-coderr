18051bbab6e19b9f487c0f82c8d4b8aa700e2f25
LeonardFDev <203679381+LeonardFDev@users.noreply.github.com>
2026-08-26 19:57:53 +0200

    fix: update settings

--- core/settings.py
+++ core/settings.py
@@ -66,11 +66,13 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+    "https://leonard-fritzmann.developerakademie.net/",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+    "https://leonard-fritzmann.developerakademie.net/",
 ]
 
 ROOT_URLCONF = 'core.urls'

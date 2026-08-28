a6250fe46a0b4d0e7bfc684d5721774e73407aff
LeonardFDev <203679381+LeonardFDev@users.noreply.github.com>
2026-08-28 23:22:07 +0200

    docs: Documentation created -> test_runner

--- core/test_runner.py
+++ core/test_runner.py
@@ -1,7 +1,12 @@
+"""Custom Django test runner configuration."""
+
 from django.test.runner import DiscoverRunner
 
 class TestProtocolClear(DiscoverRunner):
+    """before each test, the test_protocol.log file is emptied."""
+
     def setup_test_environment(self, **kwargs):
+        """Configures the test environment for test execution und empties the test_protocol.log file"""
         open("test_protocol.log", "w").close()
 
         super().setup_test_environment(**kwargs)

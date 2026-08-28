4c42d2011b9f7ce523c3799e7e4c613d323465de
LeonardFDev <203679381+LeonardFDev@users.noreply.github.com>
2026-08-28 23:50:32 +0200

    docs: Documentation created -> data_review

--- freelance_app/tests/datas/data_review.py
+++ freelance_app/tests/datas/data_review.py
@@ -1,3 +1,5 @@
+"""Data and helper function for the "Review" test"""
+
 import random
 from django.contrib.auth.models import User
 
@@ -12,6 +14,7 @@ test_review_data = {
 }
 
 def create_review_customer_objects(reviewer, range_stop = 12):
+    """creates reviews where the business user changes and the reviewer remains the same"""
     for review_number in range(1, range_stop):
         random_rating = random.choice([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
         user_business = User.objects.create_user(username=f"testuser_business(r{review_number})", password="testpassword", email=f"testuser_business(r{review_number})@test.de")
@@ -22,6 +25,7 @@ def create_review_customer_objects(reviewer, range_stop = 12):
 
 
 def create_review_business_objects(range_stop):
+    """Creates reviews where the reviewer changes and the business user remains the same"""
     user_business = User.objects.create_user(username=f"rtestuser_business", password="testpassword", email=f"rtestuser_business@test.de")
     profile_business = Profile.objects.create(user= user_business, username= user_business.username, email = user_business.email, type = "business", first_name= "Test", last_name="name")
 
@@ -32,6 +36,7 @@ def create_review_business_objects(range_stop):
         review = Review.objects.create(business_user = profile_business, reviewer = profile_customer, rating = random_rating, description = f"Alles war toll({review_number})!")
 
 def create_double_review_objects():
+    """Creates a second review by a reviewer for a business profile"""
     profile_business = Profile.objects.get(id = 2)
     profile_reviewer = Profile.objects.get(id = 1)
     double_review = Review.objects.create(business_user = profile_business, reviewer = profile_reviewer, rating = 4, description = f"Alles war toll(1)!")
